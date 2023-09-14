b6fde75f7b97b3299a02a64c4253e1accd0d16da
Nishant <ni6hant@gmail.com>
2023-09-14 10:21:17 +0530

    multiple channels

--- README.md
+++ README.md
@@ -2,13 +2,20 @@
 This extracts video links and video names for a youtube channel's video page
 
 # How To:
-Install Python, pip.
-Run Virtual environment.
-Install requirements.txt.
-Change the url in the videos.py file here: Make sure it points to the videos page and not the channel's hoempage
+Install Python.
+Copy/Download Code and open the code in Visual Studio Code root directory.
+Windows: Copy paste in VSCode Terminal. 
 ```
-# Define the URL of the YouTube channel's video page
-channel_url = 'https://www.youtube.com/@makingtechfriendlyindia/videos'
+python venv venv
+python -m venv venv
+.\venv\Scripts\activate
+pip install -r requirements.txt
+```
+
+Change the url in the videos.py file here: Make sure it points to the videos page and not the channel's homepage
+```
+# Define the URL of the YouTube channel's video page in the code itself under channels. Make sure they point to the videos page.
+
 ```
 Also increase this number based on your internet speed. 1 should be enough for most people but if you see the page not completely scrolling to the bottom change it to 2.
 ```
@@ -16,3 +23,6 @@ internet_speed = 2
 ```
 
 You can keep your terminal open on the side to see how much time is remaining for the scroll as the scroll time is dependent on the number of videos on that youtube channel.
+
+
+A new file will be created when the links are extracted. You can copy paste it directly in database or excel and it will retain it's structure since it has a tab character in between then.
--- videos.py
+++ videos.py
@@ -3,65 +3,74 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-#If your internet speed is best keep it 1, otherwise increase this. This actually changes the time till which scroll happens
-internet_speed = 2
+#If your internet speed is best keep it 1, otherwise increase this.
+#This actually changes the time till which scroll happens
+internet_speed = 1
 
-
-# Initialize Selenium WebDriver without specifying executable_path
-driver = webdriver.Chrome()
-
-# Define the URL of the YouTube channel's video page
-channel_url = 'https://www.youtube.com/@makingtechfriendlyindia/videos'
-
-# Open the channel URL
-driver.get(channel_url)
-
-# Parse the HTML content with BeautifulSoup
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'html.parser')
-
-# Find the total number of videos element by searching for its attributes
-total_videos_element = soup.find('span', {'dir': 'auto', 'class': 'style-scope yt-formatted-string'})
-
-# Extract the total number of videos and calculate the scroll duration
-total_videos_count = int(total_videos_element.text.replace(',', ''))
-scroll_duration = internet_speed *total_videos_count / 5
-print(total_videos_count, " : ", scroll_duration)
-
-# Execute JavaScript to scroll to the bottom of the page
-driver.execute_script('''
-    var scroll = setInterval(function(){
-        window.scrollBy(0, 1000);
-    }, 1000);
-''')
-
-# Initialize the progress bar
-with tqdm(total=scroll_duration, unit="s", desc="Scrolling") as pbar:
-    # Wait for the calculated duration to allow content to load
-    for _ in range(int(scroll_duration)):
-        time.sleep(1)
-        pbar.update(1)  # Update the progress bar
-
-# Get the updated page source
-updated_page_source = driver.page_source
-
-# Close the web driver
-driver.quit()
-
-# Parse the updated HTML content with BeautifulSoup
-soup = BeautifulSoup(updated_page_source, 'html.parser')
-
-# Find all video title and URL elements using the specified id and class attributes
-video_tags = soup.find_all('a', {'id': 'video-title-link', 'class': 'yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media'})
+# Define the URL of the YouTube channels' video page
+channels = [
+    'https://www.youtube.com/@ni6hant/videos',
+    'https://www.youtube.com/@ni6hantarchives/videos',
+    'https://www.youtube.com/@ni6hantgamingindia/videos',
+    'https://www.youtube.com/@makingtechfriendly/videos',
+    'https://www.youtube.com/@makingtechfriendlyindia/videos'
+]
 
 # Create a list to store video information
 video_info = []
 
-# Iterate through the video elements and extract information
-for video_tag in video_tags:
-    video_title = video_tag.get('title', 'N/A')
-    video_url = 'https://www.youtube.com' + video_tag.get('href', 'N/A')
-    video_info.append(f"{video_title}\t{video_url}")
+
+#Iterate through the YouTube Channel lists to go over them one by one:
+for channel in channels:
+    # Initialize Selenium WebDriver without specifying executable_path
+    driver = webdriver.Chrome()
+    
+    # Open the channel URL
+    driver.get(channel)
+
+    # Parse the HTML content with BeautifulSoup
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+    # Find the total number of videos element by searching for its attributes
+    total_videos_element = soup.find('span', {'dir': 'auto', 'class': 'style-scope yt-formatted-string'})
+
+    # Extract the total number of videos and calculate the scroll duration
+    total_videos_count = int(total_videos_element.text.replace(',', ''))
+    scroll_duration = internet_speed *total_videos_count / 5
+    print(total_videos_count, " : ", scroll_duration)
+
+    # Execute JavaScript to scroll to the bottom of the page
+    driver.execute_script('''
+        var scroll = setInterval(function(){
+            window.scrollBy(0, 1000);
+        }, 1000);
+    ''')
+
+    # Initialize the progress bar
+    with tqdm(total=scroll_duration, unit="s", desc="Scrolling") as pbar:
+        # Wait for the calculated duration to allow content to load
+        for _ in range(int(scroll_duration)):
+            time.sleep(1)
+            pbar.update(1)  # Update the progress bar
+
+    # Get the updated page source
+    updated_page_source = driver.page_source
+
+    # Close the web driver
+    driver.quit()
+
+    # Parse the updated HTML content with BeautifulSoup
+    soup = BeautifulSoup(updated_page_source, 'html.parser')
+
+    # Find all video title and URL elements using the specified id and class attributes
+    video_tags = soup.find_all('a', {'id': 'video-title-link', 'class': 'yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media'})
+
+    # Iterate through the video elements and extract information
+    for video_tag in video_tags:
+        video_title = video_tag.get('title', 'N/A')
+        video_url = 'https://www.youtube.com' + video_tag.get('href', 'N/A')
+        video_info.append(f"{video_title}\t{video_url}")
 
 # Write the video information to a text file
 with open('video_info.txt', 'w', encoding='utf-8') as output_file:
